712826cd7b99030732ddd7bdc39fd58fb0653a1b
Raymond <ddt_df@hotmail.com>
2017-07-08 20:45:31 +0800

    add ef test

---
+++ TryTestOnJenkins.Tests/Features/TestDBWork.feature
@@ -0,0 +1,9 @@
+﻿Feature: TestDBWork
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add a budget
+	When add budget Year "2017-01" Amount 100
+	Then the result should be a budget
---
+++ TryTestOnJenkins.Tests/Steps/TestDBWorkSteps.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+using TryTestOnJenkins.Tests.ModelsForTest;
+
+namespace TryTestOnJenkins.Tests.Steps
+{
+    [Binding]
+    [Scope(Feature = "TestDBWork")]
+    public class TestDBWorkSteps
+    {
+        [BeforeScenario()]
+        public void BeforeScenario()
+        {
+            using (var db = new GoosEntitiesForTest())
+            {
+                var budget = db.Budgets.AsQueryable();
+                db.Budgets.RemoveRange(budget);
+                db.SaveChanges();
+            }
+        }
+
+        [When(@"add budget Year ""(.*)"" Amount (.*)")]
+        public void WhenAddBudgetYearAmount(string year, int amt)
+        {
+            using (var db = new GoosEntitiesForTest())
+            {
+                var newBudget = new Budgets()
+                {
+                    Amount = amt,
+                    YearMonth = year
+                };
+
+                db.Budgets.Add(newBudget);
+                db.SaveChanges();
+            }
+        }
+        
+        [Then(@"the result should be a budget")]
+        public void ThenTheResultShouldBeABudget()
+        {
+            using (var db = new GoosEntitiesForTest())
+            {
+                var budget = db.Budgets.FirstOrDefault();
+                budget.Should().NotBeNull();
+            }
+        }
+    }
+}
