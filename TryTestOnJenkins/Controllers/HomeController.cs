9a7616119bead54eaf13afb129033fd0c4de2b8a
Raymond <ddt_df@hotmail.com>
2017-07-08 21:48:07 +0800

    add ef

--- TryTestOnJenkins/Controllers/HomeController.cs
+++ TryTestOnJenkins/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TryTestOnJenkins.Model;
 
 namespace TryTestOnJenkins.Controllers
 {
@@ -23,6 +24,11 @@ namespace TryTestOnJenkins.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            using (var db = new GoosEntities())
+            {
+                db.Budgets.AsQueryable();
+            }
 
             return View();
         }
